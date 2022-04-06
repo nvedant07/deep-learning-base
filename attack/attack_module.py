Fixed mem leakage in adv testing

attack/attack_module.py
@@ -194,12 +194,16 @@ class Attacker(ch.nn.Module):
             # Save computation (don't compute last loss) if not use_best
             if not use_best: 
                 ret = x.clone().detach()
-                return step.to_image(ret) if return_image else ret, ch.tensor(all_losses)
+            else:
+                losses, _ = calc_loss(step.to_image(x), target)
+                args = [losses, best_loss, x, best_x]
+                best_loss, ret = replace_best(*args)
 
-            losses, _ = calc_loss(step.to_image(x), target)
-            args = [losses, best_loss, x, best_x]
-            best_loss, best_x = replace_best(*args)
-            return step.to_image(best_x) if return_image else best_x, ch.tensor(all_losses)
+            # clean GPU
+            losses, loss, x = None, None, None
+            ch.cuda.empty_cache()
+
+            return step.to_image(ret) if return_image else ret, ch.tensor(all_losses)
 
         # Random restarts: repeat the attack and find the worst-case
         # example for each input in the batch
@@ -225,79 +229,3 @@ class Attacker(ch.nn.Module):
 
         return adv_ret, losses
 
-class AttackerModel(ch.nn.Module):
-    """
-    Wrapper class for adversarial attacks on models. Given any normal
-    model (a ``ch.nn.Module`` instance), wrapping it in AttackerModel allows
-    for convenient access to adversarial attacks and other applications.::
-
-        model = ResNet50()
-        model = AttackerModel(model)
-        x = ch.rand(10, 3, 32, 32) # random images
-        y = ch.zeros(10) # label 0
-        out, new_im = model(x, y, make_adv=True) # adversarial attack
-        out, new_im = model(x, y, make_adv=True, targeted=True) # targeted attack
-        out = model(x) # normal inference (no label needed)
-
-    More code examples available in the documentation for `forward`.
-    For a more comprehensive overview of this class, see 
-    :doc:`our detailed walkthrough <../example_usage/input_space_manipulation>`.
-    """
-    def __init__(self, model, dataset):
-        super(AttackerModel, self).__init__()
-        self.normalizer = utils.InputNormalize(dataset.mean, dataset.std)
-        self.model = model
-        self.attacker = Attacker(model, dataset)
-
-    def forward(self, inp, target=None, make_adv=False, with_latent=False,
-                fake_relu=False, no_relu=False, with_image=True, **attacker_kwargs):
-        """
-        Main function for running inference and generating adversarial
-        examples for a model.
-
-        Parameters:
-            inp (ch.tensor) : input to do inference on [N x input_shape] (e.g. NCHW)
-            target (ch.tensor) : ignored if `make_adv == False`. Otherwise,
-                labels for adversarial attack.
-            make_adv (bool) : whether to make an adversarial example for
-                the model. If true, returns a tuple of the form
-                :samp:`(model_prediction, adv_input)` where
-                :samp:`model_prediction` is a tensor with the *logits* from
-                the network.
-            with_latent (bool) : also return the second-last layer along
-                with the logits. Output becomes of the form
-                :samp:`((model_logits, model_layer), adv_input)` if
-                :samp:`make_adv==True`, otherwise :samp:`(model_logits, model_layer)`.
-            fake_relu (bool) : useful for activation maximization. If
-                :samp:`True`, replace the ReLUs in the last layer with
-                "fake ReLUs," which are ReLUs in the forwards pass but
-                identity in the backwards pass (otherwise, maximizing a
-                ReLU which is dead is impossible as there is no gradient).
-            no_relu (bool) : If :samp:`True`, return the latent output with
-                the (pre-ReLU) output of the second-last layer, instead of the
-                post-ReLU output. Requires :samp:`fake_relu=False`, and has no
-                visible effect without :samp:`with_latent=True`.
-            with_image (bool) : if :samp:`False`, only return the model output
-                (even if :samp:`make_adv == True`).
-
-        """
-        if make_adv:
-            assert target is not None
-            prev_training = bool(self.training)
-            self.eval()
-            adv, losses = self.attacker(inp, target, **attacker_kwargs)
-            if prev_training:
-                self.train()
-            inp = adv
-
-        normalized_inp = self.normalizer(inp)
-
-        if no_relu and (not with_latent):
-            print("WARNING: 'no_relu' has no visible effect if 'with_latent is False.")
-        if no_relu and fake_relu:
-            raise ValueError("Options 'no_relu' and 'fake_relu' are exclusive")
-
-        output = self.model(normalized_inp, with_latent=with_latent,
-                                fake_relu=fake_relu, no_relu=no_relu)
-        output_tup = (output, inp) if with_image else output
-        return (output_tup, losses) if make_adv else output_tup

attack/callbacks.py
@@ -1,7 +1,8 @@
-from typing import Union, Optional
+from typing import Union, Optional, Any
 from pytorch_lightning.callbacks import Callback
 from pytorch_lightning.trainer.trainer import Trainer
 from pytorch_lightning.core.lightning import LightningModule
+from pytorch_lightning.utilities.types import STEP_OUTPUT
 import torch as ch
 from torch.nn.modules.loss import _Loss
 from attack.attack_steps import AttackerStep
@@ -125,7 +126,7 @@ class AdvCallback(Callback):
             'model2': self.model2, 
             'targ2': self.targ2
         }
-        pl_module.__setattr__('attack_kwargs', attack_kwargs)
+        pl_module.__setattr__('attack_kwargs', attack_kwargs)        
     
     def on_test_epoch_start(self, trainer: Trainer, pl_module: LightningModule) -> None:
         ch.set_grad_enabled(True)